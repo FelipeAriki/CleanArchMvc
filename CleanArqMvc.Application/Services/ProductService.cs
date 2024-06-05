2fbba61bff67a585fae1aae91f131740eae8de9d
FelipeAriki <felipeariki3@gmail.com>
2024-06-05 11:35:44 -0300

    feat: Finalizando implementação das services e interfaces.

--- CleanArqMvc.Application/Services/ProductService.cs
+++ CleanArqMvc.Application/Services/ProductService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CleanArqMvc.Application.DTOs;
 using CleanArqMvc.Application.Interface;
+using CleanArqMvc.Domain.Entities;
 using CleanArqMvc.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -23,32 +24,37 @@ namespace CleanArqMvc.Application.Services
 
         public async Task CreateAsync(ProductDTO productDTO)
         {
-            throw new NotImplementedException();
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateAsync(productEntity);
         }
 
         public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            await _productRepository.DeleteAsync(id);
         }
 
         public async Task<IEnumerable<ProductDTO>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var productsEntity = await _productRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
         }
 
         public async Task<ProductDTO> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var productEntity = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
         }
 
         public async Task<ProductDTO> GetProductCategoryAsync(int id)
         {
-            throw new NotImplementedException();
+            var productEntity = _productRepository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
         }
 
         public async Task UpdateAsync(ProductDTO productDTO)
         {
-            throw new NotImplementedException();
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.UpdateAsync(productEntity);
         }
     }
 }
